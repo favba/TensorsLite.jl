9d944e6ce1e41edfa03bd2e12195ecd8319f4450
Documenter.jl <documenter@juliadocs.github.io>
2025-06-05 14:49:59 +0000

    build based on ec5e218

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TensorsLite","category":"page"},{"location":"#TensorsLite","page":"Home","title":"TensorsLite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TensorsLite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TensorsLite]","category":"page"}]
+[{"location":"#TensorsLite","page":"Home","title":"TensorsLite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TensorsLite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
