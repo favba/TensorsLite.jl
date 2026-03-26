f2ed41e1d885e523b26d0f7233a3df54d34a2c03
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2026-03-26 07:42:26 -0300

    Fix `isapprox` for tensors of `Complex{T}` values. Some other osmetic changes.

--- ext/LinearAlgebraExt.jl
+++ ext/LinearAlgebraExt.jl
@@ -4,7 +4,7 @@ using TensorsLite, Zeros
 
 import TensorsLite: dot, *, +, -, _muladd
 
-import LinearAlgebra: LinearAlgebra, norm
+import LinearAlgebra: LinearAlgebra, norm, ⋅, cross, normalize
 
 @inline LinearAlgebra.dot(a::AbstractTensor, b::AbstractTensor) = dot(a, b)
 @inline LinearAlgebra.dot(x::Vec, A::Ten, y::Vec) = dot(dot(x, A), y)
@@ -14,11 +14,15 @@ import LinearAlgebra: LinearAlgebra, norm
 @inline LinearAlgebra.norm(u::AbstractTensor) = fsqrt(real(inner(u, u)))
 
 @inline LinearAlgebra.norm(u::Vec1Dx, ::Real = 2) = abs(u.x)
+
 @inline LinearAlgebra.norm(u::Vec1Dy, ::Real = 2) = abs(u.y)
+
 @inline LinearAlgebra.norm(u::Vec1Dz, ::Real = 2) = abs(u.z)
+
 @inline LinearAlgebra.norm(::VecND{Zero}, ::Real = 2) = 𝟎
 
 @inline LinearAlgebra.normalize(u::AbstractTensor) = u / norm(u)
+
 @inline LinearAlgebra.normalize(u::AbstractTensor{N,Zero}) where {N} = u
 
 @inline function LinearAlgebra.cross(a::Vec, b::Vec)
@@ -31,26 +35,25 @@ import LinearAlgebra: LinearAlgebra, norm
     return  Vec(_muladd(ay, bz, -(az * by)), _muladd(az, bx, -(ax * bz)), _muladd(ax, by, -(ay * bx)))
 end
 
-@inline Base.isapprox(x::AbstractTensor{N}, y::AbstractTensor{N}) where {N} = norm(x - y) <= max(Base.rtoldefault(nonzero_eltype(x)), Base.rtoldefault(nonzero_eltype(y))) * max(norm(x), norm(y))
+base_type(::Type{T}) where {T} = T
+base_type(::Type{Complex{T}}) where {T} = T
+@inline Base.isapprox(x::AbstractTensor{N}, y::AbstractTensor{N}) where {N} = norm(x - y) <= max(Base.rtoldefault(base_type(nonzero_eltype(x))), Base.rtoldefault(base_type(nonzero_eltype(y)))) * max(norm(x), norm(y))
 
-@inline LinearAlgebra.tr(T::Ten) = T.x.x + T.y.y + T.z.z
+@inline LinearAlgebra.tr(T::Ten) = T.xx + T.yy + T.zz
 
 @inline function LinearAlgebra.det(T::Ten)
 
-    Tx = T.x
-    Txx = Tx.x
-    Tyx = Tx.y
-    Tzx = Tx.z
+    Txx = T.xx
+    Tyx = T.yx
+    Tzx = T.zx
 
-    Ty = T.y
-    Txy = Ty.x
-    Tyy = Ty.y
-    Tzy = Ty.z
+    Txy = T.xy
+    Tyy = T.yy
+    Tzy = T.zy
 
-    Tz = T.z
-    Txz = Tz.x
-    Tyz = Tz.y
-    Tzz = Tz.z
+    Txz = T.xz
+    Tyz = T.yz
+    Tzz = T.zz
 
     return _muladd((Txy*Tyz - Txz*Tyy), Tzx, _muladd((Tzy*Txz - Tzz*Txy), Tyx, (Tzz*Tyy - Tzy*Tyz )*Txx))
 
